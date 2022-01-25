a729cd91f6ff1984b484fe3c4461c9c55049b67e
AbdukhalilovIslom <abdukhalilovislom2021@gmail.com>
2022-01-26 01:20:35 +0500

    feat:add new file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(temp) {
+    temp = Math.abs(temp);
+    let a, b, c;
+    c = temp % 10;
+    b = ((temp % 100) - c) / 10;
+    a = (temp - temp % 100) / 100;
+    return 100 * c + 10 * b + a;
 }
